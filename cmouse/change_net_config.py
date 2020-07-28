a3257b83160a2e579c6dbb99f3216e49aabdf82f
Shahab <shahab.bakhtiari@gmail.com>
2020-07-28 10:58:15 -0400

    added change_net_config

---
+++ cmouse/change_net_config.py
@@ -0,0 +1,37 @@
+import network
+import mousenet
+import networkx as nx
+import numpy as np
+
+def change_net_config(net):
+
+    new_net = network.Network()
+    new_net.area_channels = net.area_channels
+    new_net.area_size = net.area_size
+
+
+    max_kernel_size = 9
+    
+    G, _ = net.make_graph()
+    Gtop = nx.topological_sort(G)
+    root = next(Gtop) # get root of graph
+    edge_bfs = [e for e in nx.edge_bfs(G, root)] 
+
+    for e in edge_bfs:
+        layer = net.find_conv_source_target(e[0],e[1])
+        params = layer.params
+        if params.kernel_size > max_kernel_size:
+            
+            params.out_channels = int(np.round((params.kernel_size / max_kernel_size) * params.out_channels))
+            params.kernel_size = max_kernel_size
+            layer.params = params
+
+        net.layers.append(layer)
+        print(e[0]+e[1],'kernel_size',params.kernel_size,'out_channels',params.out_channels)
+
+    return new_net
+
+if __name__ == "__main__":
+    
+    net = network.load_network_from_pickle('../example/network_(3,64,64).pkl')
+    new_net = change_net_config(net)
